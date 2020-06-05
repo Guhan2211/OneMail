785239f2351ce4af2b092a1d4b585f37a70f038c
Guhan <mailmeguhan98@gmail.com>
2020-06-05 13:15:35 +0530

    make it better

--- best/settings.py
+++ best/settings.py
@@ -120,13 +120,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-#STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS=[
-#      os.path.join(BASE_DIR,'static')
-#  ]
+STATICFILES_DIRS=[
+     os.path.join(BASE_DIR,'static')
+ ]
 SITE_ID=1
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL="/about"
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_REQUIRED = True
