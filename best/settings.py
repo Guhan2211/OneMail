19bdca18ec09feed7f9c71bc8734e55d74a8f24c
Guhan <mailmeguhan98@gmail.com>
2020-06-05 13:01:11 +0530

    make it better

--- best/settings.py
+++ best/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 #STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static')
+# ]
 SITE_ID=1
 
 LOGIN_REDIRECT_URL="/about"
